87daa0459629549092c7d2819c20870234f2f77b
shenchen <shenchen@debian2.(none)>
2012-06-18 11:52:33 +0800

    modify scripts

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 JEKINS_HOME="~/"
-JOBS=("zhihu-search-master" "https://github.com/ZhihuDev/search" "zhihu-search-shenchen" "https://github.com/scv119/search")
+JOBS=("zhihu-search-master" "https://github.com/ZhihuDev/search" "zhihu-search-shenchen" "https://github.com/scv119/search" "zhihu-search-sunan" "https://github.com/sunanpo/search")
 BUILD_URL="http://debian2.zhq:8080/job/"
 
 function check {
@@ -8,7 +8,7 @@ function check {
 pre_resp=("" "")
 while [ 0 -lt 1 ];do
 counter=0
-while [ $counter -lt 3 ];do
+while [ $counter -lt 5 ];do
 let uidx=counter+1
 let pidx=uidx/2
 url="http://shenchen.me:8999/fetch?url="${JOBS[$uidx]}
